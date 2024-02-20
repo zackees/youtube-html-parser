0ff18ee6b5d9bed1b88007b8225b921429ed8dae
zackees <z@zackees.com>
2024-02-19 19:31:15 -0800

    Add function to parse out up next video URLs

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -3,8 +3,11 @@ Unit test file.
 """
 from pathlib import Path
 from bs4 import BeautifulSoup
+# import beautiful soup exceptions
+from bs4 import FeatureNotFound
 import unittest
 import json
+import warnings
 
 import re
 HERE = Path(__file__).parent
@@ -28,20 +31,57 @@ def parse_out_self_video_ids(soup: BeautifulSoup) -> list[str]:
     return video_ids
 
 
+def parse_out_up_next_videos(soup: BeautifulSoup) -> list[str]:
+    """Parse out the video URL from the up next videos."""
+    video_ids: list[str] = []
+    try:
+        # div id="secondary"
+        secondary_div = soup.find("div", {"id": "secondary", "class": "ytd-watch-flexy"})
+        # now within this is div id="related"
+        related_div = secondary_div.find("div", {"id": "related"})
+        # ytd-watch-next-secondary-results-renderer
+        ytd_watch_container = related_div.find("ytd-watch-next-secondary-results-renderer")
+        # print(ytd_watch)
+        items = ytd_watch_container.find_all("ytd-compact-video-renderer")
+        for item in items:
+            try:
+                # get the <a id="thumbnail"
+                a_tag = item.find("a", {"id": "thumbnail"})
+                href = a_tag["href"]
+                video_id = href.split("=")[-1]
+                video_ids.append(video_id)
+            except FeatureNotFound as e:  # pylint: disable=broad-except
+                warnings.warn(f"Error: {e}")
+
+        # now within this is div id="related"
+        #related_div = secondary_div.find("div", {"id": "related"})
+        #print(related_div)
+        # ytd-watch-next-secondary-results-renderer
+        #ytd_watch = secondary_div.find("ytd-watch-next-secondary-results-renderer")
+        #print(ytd_watch)
+    except FeatureNotFound as e:  # pylint: disable=broad-except
+        warnings.warn(f"Error: {e}")
+    return video_ids
+
 class ParseTester(unittest.TestCase):
     """Main tester class."""
 
-    def test_first(self) -> None:
+    @unittest.skip("Not implemented yet.")
+    def test_get_self_video_id(self) -> None:
         """Just test the first element in the list."""
-        with open(TEST_HTML[0], "r", encoding="utf-8") as file:
-            soup = BeautifulSoup(file, "html.parser")
-            video_ids = parse_out_self_video_ids(soup)
-            self.assertEqual(len(video_ids), 1)
-            print(video_ids[0])
-            print()
-            #urls = parse_urls(soup)
-            #self.assertTrue(urls)
-            #print(urls)
+        test_html = TEST_HTML[0].read_text(encoding="utf-8")
+        soup = BeautifulSoup(test_html, "html.parser")
+        video_ids = parse_out_self_video_ids(soup)
+        self.assertEqual(len(video_ids), 1)
+        print(f"Found video id: {video_ids[0]}")
+        print(video_ids[0])
+
+    def test_get_up_next_videos(self) -> None:
+        test_html = TEST_HTML[0].read_text(encoding="utf-8")
+        soup = BeautifulSoup(test_html, "html.parser")
+        video_ids = parse_out_up_next_videos(soup)
+        print(f"Found {len(video_ids)} video ids.")
+        print(video_ids)
 
 
 
