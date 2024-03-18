21cddb6c160dbec8c53c80a60afc0099c9f5368c
zackees <z@zackees.com>
2024-03-17 20:06:44 -0700

    Refactor cli to handle search page parsing

--- src/youtube_html_parser/cli.py
+++ src/youtube_html_parser/cli.py
@@ -7,7 +7,12 @@ import time
 from argparse import ArgumentParser
 from pathlib import Path
 
-from youtube_html_parser.parser import parse_yt_page
+from youtube_html_parser.parser import (
+    YtPage,
+    YtPageSearch,
+    parse_yt_page,
+    parse_yt_page_seach,
+)
 
 
 def main() -> int:
@@ -15,12 +20,17 @@ def main() -> int:
     parser = ArgumentParser()
     parser.add_argument("--input-html", help="The HTML file to parse.", required=True)
     parser.add_argument("--output-json", help="The output json.", required=True)
+    parser.add_argument("--search", help="Parse a search page.", action="store_true")
     args = parser.parse_args()
     infile = Path(args.input_html)
     outfile = Path(args.output_json)
     html = infile.read_text(encoding="utf-8")
     start_time = time.time()
-    parsed = parse_yt_page(html)
+    parsed: YtPage | YtPageSearch
+    if args.search:
+        parsed = parse_yt_page_seach(html)
+    else:
+        parsed = parse_yt_page(html)
     end_time = time.time()
     print(f"Elapsed time: {end_time - start_time:.2f} seconds.")
     parsed.write_json(outfile)
